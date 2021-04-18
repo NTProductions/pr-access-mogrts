7dfc220bf1073dfa2c29ac8f4090585e97fbb807
Nate Lovell <Natelovell99@gmail.com>
2021-04-18 03:05:01 -0600

    Create access mogrts in pr.jsx

---
+++ access mogrts in pr.jsx
@@ -0,0 +1,14 @@
+﻿// access mogrts in pr
+
+var sequence = app.project.activeSequence;
+var videoTrack = sequence.videoTracks[0];
+var clipWithMogrt = videoTrack.clips[0];
+
+var mogrtComponent = clipWithMogrt.getMGTComponent();
+
+$.writeln(mogrtComponent.displayName);
+
+for(var i = 0; i < mogrtComponent.properties.numItems; i++) {
+    $.writeln(mogrtComponent.properties[i].displayName);
+    $.writeln(mogrtComponent.properties[i].getValue().toString());
+    }
